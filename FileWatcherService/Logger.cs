3671249ccd4495c6c6bf66255c59c9d45fa4a432
Vis <Vis@LAPTOP-7UL3FSUK>
2021-08-03 23:43:29 +0530

    Add project files.

---
+++ FileWatcherService/Logger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileWatcherService
+{
+   public static class Logger
+    {
+        public static void Log(string message)
+        {
+            File.AppendAllText(AppDomain.CurrentDomain.BaseDirectory + "LogFile.txt", message + Environment.NewLine);
+        }
+    }
+}
---
+++ FileWatcherService/filewatcher.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Configuration;
+namespace FileWatcherService
+{
+    class FileWatcher
+    {
+        FileSystemWatcher objWatcher;
+        public FileWatcher()
+        {
+            objWatcher = new FileSystemWatcher();
+            objWatcher.Path =ConfigurationManager.AppSettings["FilePath"] ;
+            objWatcher.Filter = "*.txt";
+            objWatcher.NotifyFilter = NotifyFilters.DirectoryName | NotifyFilters.FileName
+                | NotifyFilters.LastWrite | NotifyFilters.LastAccess;
+            objWatcher.Created += ObjWatcher_Created;
+            objWatcher.Renamed += ObjWatcher_Renamed;
+            objWatcher.Deleted += ObjWatcher_Deleted;
+            objWatcher.EnableRaisingEvents = true;
+        }
+
+        private void ObjWatcher_Deleted(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("file deleted {0}", e.FullPath));
+        }
+
+        private void ObjWatcher_Renamed(object sender, RenamedEventArgs e)
+        {
+            Logger.Log(string.Format("file Renamed from {0} to {1}", e.OldFullPath, e.FullPath));
+        }
+
+        private void ObjWatcher_Created(object sender, FileSystemEventArgs e)
+        {
+            Logger.Log(string.Format("file created {0}",e.FullPath));
+
+        }
+    }
+}
